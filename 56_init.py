4768b0edcdf036478c1667cdbbe2b91d7925b69d
ShaamNatheshwar <75720338+ShaamNatheshwar@users.noreply.github.com>
2023-09-25 22:45:47 +0530

    Update 56_init.py

--- 56_init.py
+++ 56_init.py
@@ -10,4 +10,7 @@ o1.printAll()
 
 o2 = Person("Genichiro")
 o2.printAll()
-        
+
+
+see why are we using it is....no matter how many function we call upon name...we can call it from init...so generall stuffs which will be used again and again and needs
+a new instance can be done via init
